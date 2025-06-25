09a403b472c47c60e367566f5bcb7c0d96287da4
Vinicius Coutinho <vinicius@promofarma.com.br>
2025-06-25 15:03:20 -0300

    :fix Removed vite helper

--- src/Helpers.php
+++ src/Helpers.php
@@ -4,14 +4,6 @@ declare(strict_types=1);
 
 use Viniciuscoutinh0\Minimal\Request;
 use Viniciuscoutinh0\Minimal\Response;
-use Viniciuscoutinh0\Minimal\Vite;
-
-if (! function_exists('vite')) {
-    function vite(string $path): string
-    {
-        return Vite::make()->assetUrl($path);
-    }
-}
 
 if (! function_exists('request')) {
     function request(): Request
