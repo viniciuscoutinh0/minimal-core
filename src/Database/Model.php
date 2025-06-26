10ae395629b46371597d27a9e453edffd07518b8
Vinicius Coutinho <vinicius@promofarma.com.br>
2025-06-26 13:00:15 -0300

    :feat Added attributes in model class

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -10,9 +10,35 @@ abstract class Model
 
     protected string $primaryKey = 'id';
 
+    protected array $attributes = [];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->fill($attributes);
+
+        $this->table = \Illuminate\Support\Str::singular(class_basename(static::class));
+    }
+
+    public function __get(string $key): mixed
+    {
+        return $this->attributes[$key] ?? null;
+    }
+
+    public function __set(string $key, mixed $value): void
+    {
+        $this->attributes[$key] = $value;
+    }
+
     final public static function newQuery(): QueryBuilder
     {
         return (new static)->query();
+    }
+
+    final public function fill(array $attributes): void
+    {
+        foreach ($attributes as $key => $value) {
+            $this->__set($key, $value);
+        }
     }
 
     final public function connection(): Connection
