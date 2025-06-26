ce793179f7722cd76a995847acae1c26020414c8
Vinicius Coutinho <vinicius@promofarma.com.br>
2025-06-26 14:04:33 -0300

    :fix Json falsy

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -6,6 +6,7 @@ namespace Viniciuscoutinh0\Minimal\Database;
 
 use Illuminate\Support\Collection;
 use JsonSerializable;
+use RuntimeException;
 
 abstract class Model implements JsonSerializable
 {
@@ -81,9 +82,15 @@ abstract class Model implements JsonSerializable
         return collect($this->toArray());
     }
 
-    final public function toJson(): string
+    final public function toJson(int $flags = 0, int $depth = 512): string
     {
-        return json_encode($this->toArray());
+        $json = json_encode($this->toArray(), $flags, $depth);
+
+        if ($json !== false) {
+            return $json;
+        }
+
+        throw new RuntimeException('Unable to convert model to JSON.');
     }
 
     final public function jsonSerialize(): mixed
